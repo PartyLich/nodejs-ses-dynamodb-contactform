d5bf5e64fb2c6ec1923c269854785385fa925c67
Arthur E. Jones <PartyLich@gmail.com>
2019-09-19 22:43:28 -0400

    feat: add handler implementation

---
+++ src/handler.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('dotenv').config();
+
+/**
+ * Hello world lambda response
+ * @param  {object}  event information from the invoker
+ * @return {Promise}
+ */
+const hello = async (event) => ({
+  statusCode: 200,
+  body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+  ),
+});
+
+export {
+  hello,
+};
